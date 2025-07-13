13ef202ceb3f57f9c74ce8d74816c6a81a86c4f0
Yudjin77 <yulian.khristoforov@gmail.com>
2025-07-13 16:39:38 +0300

    templates add

--- main/urls.py
+++ main/urls.py
@@ -3,8 +3,8 @@ from . import views
 
 app_name = 'main'
 
-urlpatters = [
-    path('/', views.IndexView.as_view(), name='index'),
+urlpatterns = [
+    path('', views.IndexView.as_view(), name='index'),
     path('catalog/', views.CatalogView.as_view(), name='catalog_all'),
     path('catalog/<slug:category_slug>/', views.CatalogView.as_view(), name='catalog'),
     path('product/<slug:slug>', views.ProductDetailView.as_view(), name='product_detail'),
